8029da0ccb9959d3a0751bcfc70f0119fb1121eb
Thông <46261937+nikethai@users.noreply.github.com>
2022-09-05 11:36:58 +0700

    upd model

--- model/formModel.go
+++ model/formModel.go
@@ -1,12 +1,26 @@
 package model
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Form struct {
-	ID          string `json:"id" bson:"_id,omitempty"`
-	Name        string `json:"name" bson:"name"`
-	Description string `json:"description" bson:"description"`
-	// ProjectID   string             `json:"projectId" bson:"projectId"`
-	// CreateBy    primitive.ObjectID `json:"createBy" bson:"createBy"`
-	// CreateAt    time.Time          `json:"createAt" bson:"createAt"`
-	// UpdateAt    time.Time          `json:"updateAt" bson:"updateAt"`
-	// Participants []Participant      `json:"participants" bson:"participants"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Description string             `json:"description" bson:"description"`
+	CreateAt    time.Time          `json:"createAt" bson:"createAt"`
+	UpdateAt    time.Time          `json:"updateAt" bson:"updateAt"`
+	Questions   []Question         `json:"questions" bson:"questions"`
+}
+
+func (f *Form) MarshalBSON() ([]byte, error) {
+	if f.CreateAt.IsZero() {
+		f.CreateAt = time.Now()
+	}
+	f.UpdateAt = time.Now()
+	type my Form
+	return bson.Marshal((*my)(f))
 }
--- model/projectModel.go
+++ model/projectModel.go
@@ -3,15 +3,26 @@ package model
 import (
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Project struct {
-	ID          string             `json:"id" bson:"_id,omitempty"`
-	Name        string             `json:"name" bson:"name"`
-	Description string             `json:"description" bson:"description"`
-	CreateBy    primitive.ObjectID `json:"createBy" bson:"createBy"`
-	CreateAt    time.Time          `json:"createAt" bson:"createAt"`
-	UpdateAt    time.Time          `json:"updateAt" bson:"updateAt"`
-	// Participants []Participant      `json:"participants" bson:"participants"`
+	ID           primitive.ObjectID   `json:"id" bson:"_id,omitempty"`
+	Name         string               `json:"name" bson:"name"`
+	Description  string               `json:"description" bson:"description"`
+	CreateBy     primitive.ObjectID   `json:"createBy" bson:"createBy"`
+	CreateAt     time.Time            `json:"createAt" bson:"createAt"`
+	UpdateAt     time.Time            `json:"updateAt" bson:"updateAt"`
+	Participants []primitive.ObjectID `json:"participants" bson:"participants"`
+	Forms        []primitive.ObjectID `json:"forms" bson:"forms"`
+}
+
+func (p *Project) MarshalBSON() ([]byte, error) {
+	if p.CreateAt.IsZero() {
+		p.CreateAt = time.Now()
+	}
+	p.UpdateAt = time.Now()
+	type my Project
+	return bson.Marshal((*my)(p))
 }
